7251b2be86d72f4d040372ee2c7776adbb2c79e5
Moin Khan <mo.inkhan@outlook.com>
2021-04-25 19:06:52 +0530

    ✨ v1: First Release

--- eonyx_resources/includes.js
+++ eonyx_resources/includes.js
@@ -4,7 +4,7 @@ function insertHeader() {
         <div data-collapse="medium" data-animation="default" data-duration="400" data-w-id="083b3595-12c0-3eb5-8a08-5d830155c0c6" role="banner" class="navbar w-nav" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
             <div class="w-layout-grid grid-navbar">
                 <div data-w-id="083b3595-12c0-3eb5-8a08-5d830155c0c8" class="nav" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
-                    <a href="" aria-current="page" class="brand w-nav-brand w--current" aria-label="home"><img src="eonyx_assets/601be3303f11492babdd41e2_logo.svg" loading="lazy" alt="" class="logo hide"><img src="eonyx_assets/l1t.png" style="height: 46px;" loading="lazy" alt="" class="logo"></a>
+                    <a href="./" aria-current="page" class="brand w-nav-brand w--current" aria-label="home"><img src="eonyx_assets/601be3303f11492babdd41e2_logo.svg" loading="lazy" alt="" class="logo hide"><img src="eonyx_assets/l1t.png" style="height: 46px;" loading="lazy" alt="" class="logo"></a>
                 </div>
                 <div data-w-id="083b3595-12c0-3eb5-8a08-5d830155c0cb" class="nav nav-links" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
                     <nav role="navigation" class="nav-menu w-nav-menu">
@@ -38,15 +38,17 @@ function insertFooter() {
         <div class="content">
             <div class="w-layout-grid grid-footer">
                 <div id="w-node-da92ec24-0a93-58e4-1a73-58ed5b0e2fba-5b0e2fb7" data-w-id="da92ec24-0a93-58e4-1a73-58ed5b0e2fba" class="block-footer" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;"><img src="eonyx_assets/601be3303f11492babdd41e2_logo.svg" loading="lazy" alt="" class="logo-footer hide"><img src="eonyx_assets/l1t.png" style="height: 46px; margin-bottom: 35px;" loading="lazy" alt="" class="logo">
-                    <p class="paragraph-footer">Our office:<br>137, Lane 1, <br>Iqbal Abad, Bemina - 190018<br><br><a href="mailto:info@eonyx.io">info@eonyx.io</a></p>
+                    <p class="paragraph-footer">137, Lane 1, <br>Iqbal Abad, Bemina <br> 190018<br><br><a href="mailto:info@eonyx.io">info@eonyx.io</a></p>
                 </div>
                 <div data-w-id="da92ec24-0a93-58e4-1a73-58ed5b0e2fc6" class="block-footer" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
                     <div class="heading-footer">Quick Links</div>
-                        <a href="company" aria-current="page" class="link-footer w--current">Company<br></a>
-                        <a href="features" class="link-footer">Features</a><a href="contact" class="link-footer">Contact</a>
-                        <a href="blog" class="link-footer">Blog</a>
+                        <a href="company" aria-current="page" class="link-footer w--current  hide">Company<br></a>
+                        <a href="./" class="link-footer ">Home</a>
+                        <a href="features" class="link-footer hide">Features</a>
+                        <a href="contact" class="link-footer">Contact</a>
+                        <a href="blog" class="link-footer  hide">Blog</a>
                     </div>
-                <div data-w-id="da92ec24-0a93-58e4-1a73-58ed5b0e2fd2" class="block-footer" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
+                <div data-w-id="da92ec24-0a93-58e4-1a73-58ed5b0e2fd2" class="block-footer hide" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
                     <div class="heading-footer">Social</div>
                         <a href="#" target="_blank" class="link-footer">Facebook<br></a>
                         <a href="#" target="_blank" class="link-footer">Instagram</a>
@@ -71,14 +73,14 @@ function insertFooter() {
 
 function insertStartBanner() {
     document.write(`
-    <div class="section">
+    <div class="section" id="get-started">
         <div class="content">
             <div data-w-id="d0dc812c-ec2f-fd6d-6ad9-77361725f73b" class="w-layout-grid grid-banner" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
                 <div class="text-banner">
                     <h2 class="heading white">Start your project with Eonyx</h2>
                     <p class="paragraph white">Enter your email to get started.</p>
                     <div class="form-block-banner w-form">
-                        <form id="email-form" name="email-form" data-name="Email Form" class="form-banner"><input type="email" class="text-field-white w-input" maxlength="256" name="Email" data-name="Email" placeholder="Your email" id="Email" required=""><input type="submit" value="Join!" data-wait="Please wait..." class="submit-button-white w-button"></form>
+                        <form id="email-form" name="email-form" data-name="Email Form" method="get" action="contact"  class="form-banner"><input type="email" class="text-field-white w-input" maxlength="256" name="email" data-name="Email" placeholder="Your email" id="Email" required=""><input type="submit" value="Get Started!" data-wait="Please wait..." class="submit-button-white w-button"></form>
                         <div
                             class="success-message w-form-done">
                             <div class="success-text">Thank you! Your submission has been received!</div>
