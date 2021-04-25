ef4ee531d13f9165671bb436d8b377557a667a7b
Moin Khan <mo.inkhan@outlook.com>
2021-04-25 15:14:01 +0530

    🎉 Eonyx initialized

---
+++ eonyx_resources/includes.js
@@ -0,0 +1,103 @@
+function insertHeader() {
+    document.write(`
+    <div class="section-navbar">
+        <div data-collapse="medium" data-animation="default" data-duration="400" data-w-id="083b3595-12c0-3eb5-8a08-5d830155c0c6" role="banner" class="navbar w-nav" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
+            <div class="w-layout-grid grid-navbar">
+                <div data-w-id="083b3595-12c0-3eb5-8a08-5d830155c0c8" class="nav" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
+                    <a href="" aria-current="page" class="brand w-nav-brand w--current" aria-label="home"><img src="eonyx_assets/601be3303f11492babdd41e2_logo.svg" loading="lazy" alt="" class="logo hide"><img src="eonyx_assets/l1t.png" style="height: 46px;" loading="lazy" alt="" class="logo"></a>
+                </div>
+                <div data-w-id="083b3595-12c0-3eb5-8a08-5d830155c0cb" class="nav nav-links" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
+                    <nav role="navigation" class="nav-menu w-nav-menu">
+                        <a href="./" aria-current="page" class="nav-link w-nav-link w--current">Home<br></a>
+                        <a href="features" class="nav-link w-nav-link">Features<br></a>
+                        <a href="contact" class="nav-link w-nav-link">Contact<br></a>
+                        <a href="blog" class="nav-link w-nav-link hide">Blog<br></a>
+                        <a href="company" class="nav-link w-nav-link">Company<br></a>
+                    </nav>
+                </div>
+                <div data-w-id="083b3595-12c0-3eb5-8a08-5d830155c0dc" class="nav right " style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
+                    <div class="extra-nav-menu hide">
+                        <a href="log-in" class="nav-link w-nav-link">Log in<br></a>
+                        <a href="sign-up" class="nav-button w-button">Sign up</a>
+                    </div>
+                    <div class="menu-button w-nav-button" style="-webkit-user-select: text;" aria-label="menu" role="button" tabindex="0" aria-controls="w-nav-overlay-0" aria-haspopup="menu" aria-expanded="false">
+                        <div class="menu-icon w-icon-nav-menu"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="navbar-bg" style="opacity: 1;"></div>
+            <div class="w-nav-overlay" data-wf-ignore="" id="w-nav-overlay-0"></div>
+        </div>
+    </div>
+    `)
+}
+
+function insertFooter() {
+    document.write(`
+    <div class="section-footer">
+        <div class="content">
+            <div class="w-layout-grid grid-footer">
+                <div id="w-node-da92ec24-0a93-58e4-1a73-58ed5b0e2fba-5b0e2fb7" data-w-id="da92ec24-0a93-58e4-1a73-58ed5b0e2fba" class="block-footer" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;"><img src="eonyx_assets/601be3303f11492babdd41e2_logo.svg" loading="lazy" alt="" class="logo-footer hide"><img src="eonyx_assets/l1t.png" style="height: 46px; margin-bottom: 35px;" loading="lazy" alt="" class="logo">
+                    <p class="paragraph-footer">Our office:<br>137, Lane 1, <br>Iqbal Abad, Bemina - 190018<br><br><a href="mailto:info@eonyx.io">info@eonyx.io</a></p>
+                </div>
+                <div data-w-id="da92ec24-0a93-58e4-1a73-58ed5b0e2fc6" class="block-footer" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
+                    <div class="heading-footer">Quick Links</div>
+                        <a href="company" aria-current="page" class="link-footer w--current">Company<br></a>
+                        <a href="features" class="link-footer">Features</a><a href="contact" class="link-footer">Contact</a>
+                        <a href="blog" class="link-footer">Blog</a>
+                    </div>
+                <div data-w-id="da92ec24-0a93-58e4-1a73-58ed5b0e2fd2" class="block-footer" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
+                    <div class="heading-footer">Social</div>
+                        <a href="#" target="_blank" class="link-footer">Facebook<br></a>
+                        <a href="#" target="_blank" class="link-footer">Instagram</a>
+                        <a href="#"target="_blank" class="link-footer">Twitter</a>
+                        <a href="#" target="_blank" class="link-footer">YouTube</a>
+                    </div>
+                <div data-w-id="da92ec24-0a93-58e4-1a73-58ed5b0e2fde" class="block-footer hide" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
+                    <div class="heading-footer">Template</div>
+                    <a href="template-info/instructions" class="link-footer">Instructions</a>
+                    <a href="template-info/licensing" class="link-footer">Licensing<br></a>
+                    <a href="template-info/style-guide" class="link-footer">Style Guide</a>
+                    <a href="template-info/changelog" class="link-footer">Changelog</a>
+                </div>
+            </div>
+            <div data-w-id="da92ec24-0a93-58e4-1a73-58ed5b0e2fe8" class="footer-down" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
+                <div class="text-footer-down">All rights reserved</div>
+            </div>
+        </div>
+    </div>
+    `)
+}
+
+function insertStartBanner() {
+    document.write(`
+    <div class="section">
+        <div class="content">
+            <div data-w-id="d0dc812c-ec2f-fd6d-6ad9-77361725f73b" class="w-layout-grid grid-banner" style="opacity: 0; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;">
+                <div class="text-banner">
+                    <h2 class="heading white">Start your project with Eonyx</h2>
+                    <p class="paragraph white">Enter your email to get started.</p>
+                    <div class="form-block-banner w-form">
+                        <form id="email-form" name="email-form" data-name="Email Form" class="form-banner"><input type="email" class="text-field-white w-input" maxlength="256" name="Email" data-name="Email" placeholder="Your email" id="Email" required=""><input type="submit" value="Join!" data-wait="Please wait..." class="submit-button-white w-button"></form>
+                        <div
+                            class="success-message w-form-done">
+                            <div class="success-text">Thank you! Your submission has been received!</div>
+                    </div>
+                    <div class="error-message w-form-fail">
+                        <div class="error-text">Oops! Something went wrong while submitting the form.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="img-banner">
+                <div class="block-app-screen---c"><img src="eonyx_assets/6022aa34552161bc2b3fd3da_app%2520screen%25206.svg" loading="lazy" alt="" class="app-screen---c" style="will-change: transform; transform: translate3d(0px, 30px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;"></div>
+                <div
+                    class="block-circle---c-down" style="will-change: transform; transform: translate3d(0px, 80px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;"><img src="eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle.png" loading="lazy" sizes="(max-width: 479px) 24vw, (max-width: 767px) 25vw, 24vw" srcset="eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle-p-500.png 500w, eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle.png 730w"
+                        alt="" class="image-circle---c1" style="will-change: transform; transform: translate3d(-32px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;"><img src="eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle.png"
+                        loading="lazy" sizes="9vw" srcset="eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle-p-500.png 500w, eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle.png 730w" alt="" class="image-circle---c2" style="will-change: transform; transform: translate3d(16px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;"></div>
+            <div
+                class="block-circle---c-top" style="will-change: transform; transform: translate3d(0px, 100px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;"><img src="eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle.png" loading="lazy" sizes="(max-width: 479px) 18vw, 19vw" srcset="eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle-p-500.png 500w, eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle.png 730w"
+                    alt="" class="image-circle---c3" style="will-change: transform; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;"><img src="eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle.png"
+                    loading="lazy" sizes="10vw" srcset="eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle-p-500.png 500w, eonyx_assets/60211e51b1e1bf249435035b_orange%2520circle.png 730w" alt="" class="image-circle---c4" style="will-change: transform; transform: translate3d(32px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg); transform-style: preserve-3d;"></div>
+    </div>
+    `)
+}
