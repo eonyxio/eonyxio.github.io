448165163af887b9d1a77eb987bbade097ac610f
Afaan Bilal <afaanbilal@gmail.com>
2021-04-26 20:26:59 +0530

    ✨ Contact form to Slack

---
+++ eonyx_resources/contact.js
@@ -0,0 +1,44 @@
+/**
+ * Eonyx contact form.
+ *
+ * (c) Eonyx
+ */
+
+function sendMessage(e) {
+    if (!document.forms[0].checkValidity()) {
+        document.forms[0].reportValidity()
+        return
+    }
+
+    $('#contact-loading').removeClass('hide')
+
+    let name = $('#name').val()
+    let email = $('#email').val()
+    let phone = $('#phone').val()
+    let message =  $('#message').val()
+
+    fetch('https://script.google.com/macros/s/AKfycbyXhkQEBIeyYlrGilH2ClsoyBWqKJ2A7aUHrSZbbBVoprrfsfEKW4XVshaVSf2i68d4/exec',
+        {
+            method:'post',
+            headers: {
+                'Content-Type':'application/x-www-form-urlencoded'
+            },
+            body: 'name=' + encodeURIComponent(name)
+                + '&email=' + encodeURIComponent(email)
+                + '&message=' + encodeURIComponent(message)
+                + '&phone=' + encodeURIComponent(phone)
+            })
+            .then(r => r.text())
+            .then(d => {
+                $('#contact-loading').addClass('hide')
+                if (d == 'success') {
+                    $('#contact-success').removeClass('hide')
+                    $('#name').val('')
+                    $('#email').val('')
+                    $('#phone').val('')
+                    $('#message').val('')
+                } else {
+                    $('#contact-error').removeClass('hide')
+                }
+            })
+}
