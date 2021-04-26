1f02d2896dc68f9d43ae9fadfad99bd0331fcc14
Afaan Bilal <afaanbilal@gmail.com>
2021-04-27 04:35:50 +0530

    🎨 Format

--- eonyx_resources/contact.js
+++ eonyx_resources/contact.js
@@ -15,30 +15,30 @@ function sendMessage(e) {
     let name = $('#name').val()
     let email = $('#email').val()
     let phone = $('#phone').val()
-    let message =  $('#message').val()
+    let message = $('#message').val()
 
     fetch('https://script.google.com/macros/s/AKfycbyXhkQEBIeyYlrGilH2ClsoyBWqKJ2A7aUHrSZbbBVoprrfsfEKW4XVshaVSf2i68d4/exec',
         {
-            method:'post',
+            method: 'post',
             headers: {
-                'Content-Type':'application/x-www-form-urlencoded'
+                'Content-Type': 'application/x-www-form-urlencoded'
             },
             body: 'name=' + encodeURIComponent(name)
                 + '&email=' + encodeURIComponent(email)
                 + '&message=' + encodeURIComponent(message)
                 + '&phone=' + encodeURIComponent(phone)
-            })
-            .then(r => r.text())
-            .then(d => {
-                $('#contact-loading').addClass('hide')
-                if (d == 'success') {
-                    $('#contact-success').removeClass('hide')
-                    $('#name').val('')
-                    $('#email').val('')
-                    $('#phone').val('')
-                    $('#message').val('')
-                } else {
-                    $('#contact-error').removeClass('hide')
-                }
-            })
+        })
+        .then(r => r.text())
+        .then(d => {
+            $('#contact-loading').addClass('hide')
+            if (d == 'success') {
+                $('#contact-success').removeClass('hide')
+                $('#name').val('')
+                $('#email').val('')
+                $('#phone').val('')
+                $('#message').val('')
+            } else {
+                $('#contact-error').removeClass('hide')
+            }
+        })
 }
