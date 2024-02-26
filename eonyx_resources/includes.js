9f54c381cd983e481c9eb8e28d2a4966074a9970
Zahid beigh <124980915+ZahidBeigh123@users.noreply.github.com>
2024-02-26 10:24:11 +0530

    Update eonyx_resources/includes.js
    Co-authored-by: Afaan Bilal <AfaanBilal@users.noreply.github.com>

--- eonyx_resources/includes.js
+++ eonyx_resources/includes.js
@@ -458,7 +458,7 @@ function insertPortfolio() {
                         </div>
                     </div>
                 </div>
-            </div>     
+            </div>
         </div>
     </div>
     `)
